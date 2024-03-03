c2e95112fe2ef29161bb4770c8f3249bfdc02f94
mvalladares-2019607 <mvalladares-2019607@kinal.edu.gt>
2024-03-03 10:34:42 -0600

    se creó el modelo de publications/post

---
+++ src/Publications/post.model.js
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose';
+
+const postSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  category: {
+    type: String,
+    required: true
+  },
+  content: {
+    type: String, 
+    required: true
+  },
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+const Post = mongoose.model('Post', postSchema);
+
+export default Post;
